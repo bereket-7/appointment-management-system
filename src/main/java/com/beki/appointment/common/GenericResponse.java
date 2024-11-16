8dee285e1a8d3beee4dac8c2f13f0dbe2d5f1b98
bereket-7 <bekhonelign@gmail.com>
2024-11-16 21:29:51 +0300

    GenericResponse

---
+++ src/main/java/com/beki/appointment/common/GenericResponse.java
@@ -0,0 +1,12 @@
+package com.beki.appointment.common;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+
+import java.io.Serializable;
+
+@Builder
+public record GenericResponse<T>(@JsonProperty("status") int status,
+                                 @JsonProperty("message") String message,
+                                 @JsonProperty("content") T content) implements Serializable {
+}
