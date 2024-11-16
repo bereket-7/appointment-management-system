043ef1af9c336a037db93ed751a84e157cedff58
bereket-7 <bekhonelign@gmail.com>
2024-11-16 21:33:21 +0300

    UserRepository

---
+++ src/main/java/com/beki/appointment/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.beki.appointment.repository;
+
+import com.beki.appointment.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
+}
