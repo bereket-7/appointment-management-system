2b37f24155fbde92cd8e9b528fdc7748286006f2
bereket-7 <bekhonelign@gmail.com>
2024-11-16 23:29:52 +0300

    auth controller

--- src/main/java/com/beki/appointment/controller/AuthController.java
+++ src/main/java/com/beki/appointment/controller/AuthController.java
@@ -2,8 +2,13 @@ package com.beki.appointment.controller;
 
 import com.beki.appointment.common.LoginRequest;
 import com.beki.appointment.common.LoginResponseDto;
+import com.beki.appointment.common.UserRegistrationDto;
+import com.beki.appointment.exception.GeneralException;
+import com.beki.appointment.model.User;
 import com.beki.appointment.security.CustomUserDetails;
 import com.beki.appointment.security.JwtTokenProvider;
+import com.beki.appointment.service.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -17,13 +22,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthController {
-
     private final AuthenticationManager authenticationManager;
     private final JwtTokenProvider jwtTokenProvider;
+    private final UserService userService;
 
-    public AuthController(AuthenticationManager authenticationManager, JwtTokenProvider jwtTokenProvider) {
+
+    public AuthController(AuthenticationManager authenticationManager, JwtTokenProvider jwtTokenProvider, UserService userService) {
         this.authenticationManager = authenticationManager;
         this.jwtTokenProvider = jwtTokenProvider;
+        this.userService = userService;
+    }
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody UserRegistrationDto request) {
+    try
+    {
+        User user = userService.registerUser(request);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+    catch (GeneralException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 
     @PostMapping("/login")
