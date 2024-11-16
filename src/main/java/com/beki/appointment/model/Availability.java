5373bd2f84acaf0599d2ebb5ac8b013c47e1603d
bereket-7 <bekhonelign@gmail.com>
2024-11-16 23:44:06 +0300

    Availability

---
+++ src/main/java/com/beki/appointment/model/Availability.java
@@ -0,0 +1,30 @@
+package com.beki.appointment.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "AVAILABILITY")
+public class Availability {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "provider_id", nullable = false)
+    private Long providerId;
+
+    @Column(name = "start_time", nullable = false)
+    private LocalDateTime startTime;
+
+    @Column(name = "end_time", nullable = false)
+    private LocalDateTime endTime;
+
+    @Column(name = "is_booked", nullable = false)
+    private boolean isBooked = false;
+
+}
