92bcca5bf7fd0440b9045363584493e520a9c63c
bereket-7 <bekhonelign@gmail.com>
2024-11-16 21:30:34 +0300

    JwtTokenProvider

---
+++ src/main/java/com/beki/appointment/security/JwtTokenProvider.java
@@ -0,0 +1,38 @@
+package com.beki.appointment.security;
+
+import io.jsonwebtoken.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+    private final String jwtSecret = "hY5Kd8VkGz9sQ3PqN5RtZj4Tl2ApMj7KwXyB8SfL0VcN1HbD6RdX";
+    private final int jwtExpirationMs = 86400000; // 1 day
+
+    public String generateToken(Authentication authentication) {
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            throw new RuntimeException("Invalid JWT token", e);
+        }
+    }
+
+    public String getUserFromJWT(String token) {
+        Claims claims = Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody();
+        return claims.getSubject();
+    }
+}
