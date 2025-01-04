6fe82f4d596729c0862d6fc387ec8977bf40dfd5
bereket-7 <bekhonelign@gmail.com>
2025-01-05 00:28:42 +0300

    ServiceProviderRepository

---
+++ src/main/java/com/beki/appointment/repository/ServiceProviderRepository.java
@@ -0,0 +1,9 @@
+package com.beki.appointment.repository;
+
+import com.beki.appointment.model.ServiceProvider;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ServiceProviderRepository extends JpaRepository<ServiceProvider, Long> {
+}
