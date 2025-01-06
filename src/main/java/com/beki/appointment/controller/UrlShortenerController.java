6b23c6ecaf226c7c430e309574ca0300b9ed18cb
bereket-7 <bekhonelign@gmail.com>
2025-01-06 21:45:18 +0300

    filter url controller

--- src/main/java/com/beki/appointment/controller/UrlShortenerController.java
+++ src/main/java/com/beki/appointment/controller/UrlShortenerController.java
@@ -1,9 +1,14 @@
 package com.beki.appointment.controller;
 
+import com.beki.appointment.model.UrlMapping;
 import com.beki.appointment.service.UrlShortenerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/url")
@@ -29,5 +34,14 @@ public class UrlShortenerController {
     @DeleteMapping("/{id}")
     public void deleteShortUrl(@PathVariable Long id) {
          urlShortenerService.deleteByShortUrl(id);
+    }
+
+    @GetMapping("/filter")
+    public List<UrlMapping> filterUrlMappings(
+            @RequestParam(required = false) Long providerId,
+            @RequestParam(required = false) String originalUrl,
+            @RequestParam(required = false) String shortUrl,
+            @RequestParam(required = false) Timestamp createdDate) {
+        return urlShortenerService.filterUrls(providerId, originalUrl, shortUrl, createdDate);
     }
 }
