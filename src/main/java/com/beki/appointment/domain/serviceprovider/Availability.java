b42f704abe31d3f9d71e37096f99fcbf25a047f7
Bereket Honelign <bekhonelign@gmail.com>
2026-03-24 15:02:20 +0300

    refactor: restructure project to align with clean architecture principles by introducing domain, infrastructure, and interfaces layers.

---
+++ src/main/java/com/beki/appointment/domain/serviceprovider/Availability.java
@@ -0,0 +1,37 @@
+package com.beki.appointment.domain.serviceprovider;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.DayOfWeek;
+
+@Entity
+@Getter
+@Setter
+@Data
+@Table(name = "AVAILABILITY", indexes = {
+        @Index(name = "idx_availability_id", columnList = "id, PROVIDER_ID")
+})
+public class Availability {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "PROVIDER_ID", nullable = false)
+    private Long providerId;
+
+
+    @Column(name = "day_of_week", nullable = false)
+    private DayOfWeek dayOfWeek;
+
+    @Column(name = "START_TIME")
+    private LocalDateTime startTime;
+
+    @Column(name = "END_TIME")
+    private LocalDateTime endTime;
+
+    @Column(name = "IS_BOOKED", nullable = false)
+    private boolean isBooked = false;
+
+}
