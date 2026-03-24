d025eaf0409d216c61c33cc40ec0f15eea5d898f
Bereket Honelign <bekhonelign@gmail.com>
2026-03-24 15:15:37 +0300

    refactor: restructure project into layered architecture with dedicated interfaces, infrastructure, and shared packages.

---
+++ src/main/java/com/beki/appointment/infrastructure/persistence/UserRepository.java
@@ -0,0 +1,14 @@
+package com.beki.appointment.infrastructure.persistence;
+
+import com.beki.appointment.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
